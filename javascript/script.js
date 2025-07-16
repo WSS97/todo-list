f8aa028df12fce6edad6a65b745930bef5c68b5d
Wesley <129167492+WSS97@users.noreply.github.com>
2025-07-16 11:20:46 -0300

    correcao/criacao de funcionalidade
    Alteracao da funcao com innerHTML por createElement e appendChild, mais criacao da funcao para salvar tarefa editada

--- javascript/script.js
+++ javascript/script.js
@@ -17,27 +17,69 @@ window.onload = () => {
     }
     // criacao do elemnto representante da tarefa
     function cadastro() {
-      main.innerHTML += `<div class="task">
-            <input type="text" value="${inTask.value}" disabled />
-            <button class="edit">Editar</button>
-        <button onclick="remove()">Excluir</button>
-        <button onclick="ok()">ok</button>
-        </div>`;
-      inTask.value = "";
+      const divTask = document.createElement("div");
+      divTask.classList.add("task");
+      const inputTask = document.createElement("input");
+      const buttonEdit = document.createElement("button");
+      const buttonRemove = document.createElement("button");
+      const buttonOk = document.createElement("button");
+      const buttonSave = document.createElement("button");
+      //  adcionando funcao ao botao editar
+      buttonEdit.onclick = () => {
+        inputTask.removeAttribute("disabled");
+        inputTask.focus();
+        inputTask.setSelectionRange(
+          inputTask.value.length,
+          inputTask.value.length
+        );
+
+        buttonEdit.classList.add("noView");
+        buttonOk.classList.add("noView");
+        buttonRemove.classList.add("noView");
+
+        buttonSave.classList.remove("noView");
+      };
+      // funcao salvar tarefa editada
+      buttonSave.onclick = () => {
+        inputTask.setAttribute("disabled", true);
+        inputTask.blur();
+        buttonEdit.classList.remove("noView");
+        buttonOk.classList.remove("noView");
+        buttonRemove.classList.remove("noView");
+
+        buttonSave.classList.add("noView");
+      };
+
+      inputTask.value = inTask.value;
+      inputTask.setAttribute("disabled", true);
+      buttonEdit.textContent = "Editar";
+      buttonRemove.textContent = "Excluir";
+      buttonOk.textContent = "Ok";
+      buttonSave.textContent = "Salvar";
+      buttonSave.classList.add("noView");
+
+      divTask.appendChild(inputTask);
+      divTask.appendChild(buttonEdit);
+      divTask.appendChild(buttonRemove);
+      divTask.appendChild(buttonOk);
+      divTask.appendChild(buttonSave);
+
+      main.appendChild(divTask);
     }
   }
   // passa por cada botao de edicao e adciona a funcao para editar o elemento da tarefa na lista
-  editButtons.forEach((btn) => {
-    btn.onclick = () => {
-      const tasks = btn.closest(".task");
-      const input = tasks.querySelector("input");
-      const length = input.value.length;
+  //   editButtons.forEach((btn) => {
+  //     btn.onclick = () => {
+  //       const tasks = btn.closest(".task");
+  //       const input = tasks.querySelector("input");
+  //       const length = input.value.length;
 
-      input.removeAttribute("disabled");
-      input.focus();
-      input.setSelectionRange(length, length);
+  //       input.removeAttribute("disabled");
+  //       input.focus();
+  //       input.setSelectionRange(length, length);
 
-      btn.textContent = "Salvar";
-    };
-  });
+  //       btn.textContent = "Salvar";
+  //       btn.classList.remove("task");
+  //     };
+  //   });
 };
